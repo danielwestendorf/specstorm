3fd721b0e5e105159a71d2cf51406e59e02c2994
Daniel Westendorf <daniel@prowestech.com>
2025-04-01 08:20:52 -0600

    Generate gem

---
+++ lib/specstorm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "specstorm/version"
+
+module Specstorm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/specstorm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Specstorm do
+  it "has a version number" do
+    expect(Specstorm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
